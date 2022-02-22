fa27cf39b09cf57ce3e71e503da62756185d1d7c
prashantbharati92@gmail.com <prashantbharati92@gmail.com>
2022-02-22 17:18:27 +0530

    form styles

---
+++ src/styles.js
@@ -0,0 +1,12 @@
+import { makeStyles } from "@material-ui/core/styles";
+
+export default makeStyles((theme) => ({
+  form: {
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "center",
+    // alignItems: "center",
+
+    gap: "30px",
+  },
+}));
